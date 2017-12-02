d5cf4465e16df698267a51f0762b9f2e0f4e8681
RaoulMeyer <raouljmeyer@gmail.com>
2017-12-02 22:48:45 +0100

    Day 1

---
+++ 01/part1.cpp
@@ -0,0 +1,30 @@
+#include <sstream>
+#include <fstream>
+#include <streambuf>
+#include <iostream>
+#include <vector>
+
+std::string read_input() {
+    std::ifstream t("input");
+    std::stringstream buffer;
+    buffer << t.rdbuf();
+
+    return buffer.str();
+}
+
+int main() {
+    std::string input = read_input();
+
+    const std::vector<char> chars(input.begin(), input.end());
+
+    int sum = 0;
+    for (int i = 0; i < chars.size(); i++) {
+        if (chars.at((i + 1) % chars.size()) == chars.at(i)) {
+            sum += chars.at(i) - '0';
+        }
+    }
+
+    std::cout << sum << std::endl;
+
+    return 0;
+}
